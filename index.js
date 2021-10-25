7f17e897194757d1081975d13c67961ab819d974
Yu4iy <yurimohyliuk@gmail.com>
2021-10-25 18:19:32 +0200

    add script

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('test')
+const boxes = document.querySelector('.boxes');
+for(let i = 1; i <= 100; i++) {
+
+	if (i % 3 === 0 && i % 5 === 0 ) {
+		boxes.innerHTML += `<div class="box fizzbuzz">fizzbuzz</div>`;
+		}
+		else if (i % 3 === 0) {
+			boxes.innerHTML += `<div class="box fizz">fizz</div>`;
+		}
+		else if (i % 5 === 0) {
+			boxes.innerHTML += `<div class="box buzz">buzz</div>`;
+		} 
+		else {
+			boxes.innerHTML += `<div class="box ">${i}</div>`;
+		}	
+}
